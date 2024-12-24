d6f1bef6f6a4283a91cfe78bde2b94c493265152
LiR0d <dima.soshnikov51@gmail.com>
2024-12-24 14:27:35 +0300

    Add files via upload

---
+++ labs/lab_6/JSWingsOfToath.js
@@ -0,0 +1,23 @@
+function showWarning() {
+    alert("Здесь будет ссылка, которая на данный момент не реализована.");
+}
+
+function showLocationAlert() {
+    alert("Вы выбрали: Россия, Москва.");
+}
+
+function showShopsAlert() {
+    alert("Информация о магазинах появится позже.");
+}
+
+function showDeliveryAlert() {
+    alert("Информация о доставке и оплате будет добавлена.");
+}
+
+function showBuyAlert() {
+    alert("Спасибо за интерес к нашему магазину! Функционал покупки пока не реализован.");
+}
+
+function showFooterAlert() {
+    alert("Все права защищены. ©Крылья Тота.");
+}
